0ad3eb491ca71d3576d85d3e10661322ab635345
Jonathan <70845195+JonathanOll@users.noreply.github.com>
2025-11-12 17:38:12 +0100

    path finding npc

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 @export var navigation_region: NavigationRegion3D
+@export var player: Player
+
+var npc: NPC
 
 func _ready() -> void:
 	for i in range(len(Global.products)):
@@ -9,23 +12,30 @@ func _ready() -> void:
 		
 		var box = Box.create(product)
 		box.position = Vector3(0, 10 + 2*i, -10)
-		add_child(box)
+		navigation_region.add_child(box)
 	
 	var rack = Rack.create()
 	rack.position = Vector3(-8, 0, -22)
-	add_child(rack)
+	navigation_region.add_child(rack)
 	
 	var aisle = Aisle.create()
 	aisle.position = Vector3(-8, 0, -5)
-	add_child(aisle)
+	navigation_region.add_child(aisle)
 	
 	var checkout_counter = CheckoutCounter.create()
 	checkout_counter.position = Vector3(7, 0, 11)
-	add_child(checkout_counter)
+	navigation_region.add_child(checkout_counter)
 	
-	var npc = NPC.create()
+	npc = NPC.create()
 	npc.position = Vector3(10, 10, -10)
 	navigation_region.add_child(npc)
 	
+	navigation_region.bake_navigation_mesh()
+	
 	npc.go_to(Vector3(-10, 0, 10))
+	
+func _process(delta):
+	if Input.is_action_pressed("dev"):
+		npc.go_to(player.global_position)
+	
 	
--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -11,17 +11,25 @@ func _ready() -> void:
 	# TODO: créer une liste de course, pathfinding (entre rayons, a la caisse...)
 
 func _physics_process(delta: float) -> void:
+	velocity.x = 0
+	velocity.z = 0
 	if not is_on_floor():
 		velocity.y -= Global.gravity * delta
-	var pos = agent.get_next_path_position()
-	var dir = (pos - position).normalized()
-	velocity.x = dir.x * SPEED
-	velocity.z = dir.z * SPEED
+	if !agent.is_target_reached():
+		follow_path()
+	
 	move_and_slide()
 
 func go_to(pos: Vector3):
 	agent.target_position = pos
-	print(agent.get_current_navigation_path())
+
+func follow_path():
+	var pos = agent.get_next_path_position()
+	var dir = (pos - global_position).normalized()
+	velocity.x = dir.x * SPEED
+	velocity.z = dir.z * SPEED
 
 static func create() -> NPC:
 	return scene.instantiate()
+
+	
